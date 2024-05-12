6b411ce26bc1b1fc0489c511f5842ea74a929710
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-12 16:40:09 +0200

    feat: Add SQLBase and EngineSingleton classes

--- src/pzsm/db/__init__.py
+++ src/pzsm/db/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa: F401
 from __future__ import annotations
 
+from .sql_engine import EngineSingleton
 from .sql_mod import SQLMod
 from .sql_mod_id import SQLModID
 from .sql_utilities import sync, update_enabled
