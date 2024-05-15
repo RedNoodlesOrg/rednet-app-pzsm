089f30c0ccd9f0d05484255f6aaad20fc8eb65b0
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-15 19:15:58 +0200

    feat: Refactor Modlist class and update documentation
    The `Modlist` class in the `modlist.py` file has been refactored to improve its functionality and readability. The code changes include:
    - Adding type hints to the class attributes and method signatures for better code clarity.
    - Updating the `__init__` method to handle different combinations of input parameters.
    - Modifying the `get_ids` method to raise a `ValueError` if the mods are not set.
    - Updating the documentation of the class and its attributes.
    
    These changes enhance the usability and maintainability of the `Modlist` class.

--- src/pzsm/file_parser/modlist.py
+++ src/pzsm/file_parser/modlist.py
@@ -6,35 +6,49 @@ from SteamAPI.mod import Mod
 
 
 class Modlist:
-    """Modlist."""
+    """
+    Represents a list of game mods with functionalities to manage and retrieve mod and workshop IDs.
+
+    Attributes:
+        mods ( list[Mod] | None): List of mod objects.
+        modids (str | None): Concatenated string of mod IDs.
+        workshopids (str | None): Concatenated string of workshop IDs.
+    """
 
     mods: list[Mod] | None
     modids: str | None
     workshopids: str | None
 
     def __init__(self, mods: list[Mod] | None = None, modids: str | None = None, workshopids: str | None = None):
-        """Init of Modlist.
+        """Initialize the Modlist.
 
         Args:
             mods (list[Mod] | None, optional): List of mods. Defaults to None.
-            modids (str | None, optional): List of modid's. Defaults to None.
-            workshopids (str | None, optional): List of workshop id's. Defaults to None.
+            modids (str | None, optional): List of mod IDs. Defaults to None.
+            workshopids (str | None, optional): List of workshop IDs. Defaults to None.
 
         Raises:
-            ValueError: Occurs when either both mods and ids are None or both are set.
+            ValueError: Occurs when either both mods and IDs are None or both are set.
         """
         if mods is None:
             if modids is None or workshopids is None:
-                raise ValueError("Cannot leave out mods and ids!")
+                raise ValueError("Cannot leave out mods and IDs!")
         else:
             if modids is not None or workshopids is not None:
-                raise ValueError("Cannot set mods and ids!")
+                raise ValueError("Cannot set mods and IDs!")
         self.mods = mods
         self.modids = modids
         self.workshopids = workshopids
 
     def get_ids(self) -> tuple[str, str]:
-        """get_modids."""
+        """Get the mod IDs and workshop IDs.
+
+        Returns:
+            A tuple containing the mod IDs and workshop IDs as strings.
+
+        Raises:
+            ValueError: If the mods are not set.
+        """
         if self.modids is None or self.workshopids is None:
             if self.mods:
                 mod_ids = []
@@ -45,5 +59,5 @@ class Modlist:
                 self.modids = ";".join(mod_ids)
                 self.workshopids = ";".join([mod.workshop_id for mod in self.mods])
             else:
-                raise ValueError("mods not set!")
+                raise ValueError("Mods not set!")
         return self.modids, self.workshopids
